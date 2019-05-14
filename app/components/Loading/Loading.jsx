2d24db9e3f275eb8b9e1b23628482e2c23840612
David S. Brima <david@brima.it>
2019-05-14 12:59:00 +0300

    Added Loading Component

---
+++ app/components/Loading/Loading.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import classNames from 'classnames'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+
+export default function Loading({ className, loadingText, ...props }) {
+  return (
+    <div className={classNames('Loading', className)} {...props}>
+      <span>
+        <FontAwesomeIcon icon={['far', 'spinner']} spin />
+        {loadingText}
+      </span>
+    </div>
+  )
+}
+
+Loading.propTypes = {
+  loadingText: PropTypes.string,
+  className: PropTypes.string,
+}
+
+Loading.defaultProps = {
+  loadingText: ' Loading...',
+}
