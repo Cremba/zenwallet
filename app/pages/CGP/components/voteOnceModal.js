627e055fb1b45ed89112b0534d18356939c95623
Lior Frenkel <frenkeldev@gmail.com>
2019-09-09 10:51:33 +0200

    chore: change text

--- app/pages/CGP/components/voteOnceModal.js
+++ app/pages/CGP/components/voteOnceModal.js
@@ -6,12 +6,25 @@ export default function voteOnceModal() {
   const container = document.createElement('div')
   container.className = 'vote-once-modal-content'
   container.innerHTML = `
-    <ul>
-      <li>You can only vote once for CGP Allocation and once for CGP Payout</li>
-      <li>You can vote separately</li>
-      <li>Your first vote is the one which counts</li>
-      <li>Your vote weight will consist of your total ZP at the snapshot block</li>
-    </ul>
+    <div class="align-left" style="white-space: nowrap;">
+      <p>
+        <span>Each month the community may vote for:</span><br />
+        <ul>
+          <li>The amount of ZP allocated to the CGP</li>
+          <li>The recipient of the PayoutTx</li>
+        </ul>
+        <br />
+        You vote separately for each, only the first vote counts.
+      </p>
+      <br />
+      <p>
+        The weight of your vote is a function of: <br />
+        <ul>
+          <li>Your ZP balance at the snapshot block</li>
+          <li>The total amount of ZP which participated in the vote</li>
+        </ul>
+      </p>
+    </div>
   `
 
   return swal({
